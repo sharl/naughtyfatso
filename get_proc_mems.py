ae6bbd338477a0519f2c20eff1aae1b7103c267b
Sharl Morlaroll <sharl@hauN.org>
2026-06-29 12:31:09 +0900

    fix return value / optimized

--- get_proc_mems.py
+++ get_proc_mems.py
@@ -2,13 +2,21 @@
 from psutil import process_iter, NoSuchProcess, AccessDenied, ZombieProcess
 
 
-def get_proc_mems() -> list:
+def get_proc_mems() -> dict[str, int]:
     procs = {}
 
     for proc in process_iter(['name', 'memory_info']):
         try:
-            name = proc.name().lower()
-            mem = proc.memory_info().rss
+            name = proc.info['name']
+            if name is None:
+                continue
+            name = name.lower()
+
+            mem_info = proc.info['memory_info']
+            if mem_info is None:
+                continue
+            mem = mem_info.rss
+
             if name not in procs:
                 procs[name] = 0
             procs[name] += mem
